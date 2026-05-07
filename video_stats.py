b76392b6a1ca001d67778d439aeab96b302a79ec
Adrian Chen <adrianchen@mac.home>
2026-05-07 22:30:13 +0200

    video_stats.py

--- video_stats.py
+++ video_stats.py
@@ -9,6 +9,7 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
 Channel_handle = "MrBeast"
+maxResults = 50
 
 url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={Channel_handle}&key={API_KEY}"
 
@@ -27,5 +28,44 @@ def get_playlist_id():
         return None
     
 
+# baseURL = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId=UUX6OQ3DkcsbYNE6H8uQQuVA&key={API_KEY}'
+
+# playlist_id = get_playlist_id()
+
+def get_video_ids(playlist_id):
+
+    video_ids =[]
+
+    pageToken = None
+
+    baseURL = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlist_id}&key={API_KEY}'
+
+    try:
+        while True:
+            url = baseURL
+            if pageToken:
+                url += f'&pageToken={pageToken}'
+            
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break
+        
+        return video_ids
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
+
 if __name__ == "__main__":
-    get_playlist_id()
+    playlist_id = get_playlist_id()
+    print(get_video_ids(playlist_id))
